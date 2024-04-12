fc0528dc9f9389e1cf9149ba0d7a7654cff56fe9
tiszczesz <tiszczesz@gmail.com>
2024-04-12 15:57:53 +0200

    gg

--- cw2/src/app/app.routes.ts
+++ cw2/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { Cw1Component } from './cw1/cw1.component';
+import { Cw2Component } from './cw2/cw2.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:HomeComponent},
+    {path:'cw1',component:Cw1Component},
+    {path:'cw2',component:Cw2Component}
+];
