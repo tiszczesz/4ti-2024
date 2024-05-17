c6de023e791e5eba79de4292fdedbeb6455c7ab7
tiszczesz <tiszczesz@gmail.com>
2024-05-17 15:33:55 +0200

    ggg

--- cw3/src/app/cw2/cw2.component.ts
+++ cw3/src/app/cw2/cw2.component.ts
@@ -8,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './cw2.component.css'
 })
 export class Cw2Component {
+  handleInput($event: Event) {
+    this.text2 = ($event.target as HTMLTextAreaElement)?.value
+  }
+  text: String = "";
+  text2="";
+  handelChange($event: Event) {
+    this.text = ($event.target as HTMLTextAreaElement)?.value
+  }
 
 }
