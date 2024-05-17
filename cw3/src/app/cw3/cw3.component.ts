0b9624372f6634c97307a6c65a351d7e98ce56c0
tiszczesz <tiszczesz@gmail.com>
2024-05-17 15:58:39 +0200

    gg

--- cw3/src/app/cw3/cw3.component.ts
+++ cw3/src/app/cw3/cw3.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule, } from '@angular/forms';
 
 @Component({
   selector: 'app-cw3',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './cw3.component.html',
   styleUrl: './cw3.component.css'
 })
 export class Cw3Component {
+name: any;
 
 }
